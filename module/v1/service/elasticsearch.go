b8e4e2c410f6360aa4af6dc2de7eff90b979d069
yaya rizqi isnanda <yaya.rizqi.isnanda@gmail.com>
2020-03-09 15:39:36 +0700

    feature: add news service and elasticsearch service

--- module/v1/service/elasticsearch.go
+++ module/v1/service/elasticsearch.go
@@ -1,1 +1,141 @@
 package services
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	models "github.com/yaya-ri/API-news/module/v1/model"
+)
+
+//ElasticSearchService godoc
+type ElasticSearchService struct {
+	host string
+	port int
+	http *http.Client
+}
+
+//ElasticSearchServiceHandler godoc
+func ElasticSearchServiceHandler(host string, port int) ElasticSearchServiceInterface {
+	return &ElasticSearchService{
+		host: host,
+		port: port,
+		http: &http.Client{},
+	}
+}
+
+//CheckIndexExist check if index exist in elasticsearch
+func (service *ElasticSearchService) CheckIndexExist(name string) (bool, error) {
+	url := fmt.Sprintf("http://%s:%d/%s", service.host, service.port, name)
+
+	request, err := http.NewRequest(http.MethodHead, url, nil)
+	if err != nil {
+		return false, err
+	}
+
+	res, err := service.http.Do(request)
+	if err != nil {
+		return false, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return false, err
+	}
+
+	return true, err
+}
+
+//CreateIndex create new index for elasticsearch
+func (service *ElasticSearchService) CreateIndex(name string, maps map[string]interface{}) error {
+	url := fmt.Sprintf("http://%s:%d/%s", service.host, service.port, name)
+	bodyRequest, err := json.Marshal(maps)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(bodyRequest))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		return err
+	}
+
+	res, err := service.http.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("Failed create index %s", name)
+	}
+	return nil
+}
+
+//Store save document to elasticsearch
+func (service *ElasticSearchService) Store(index, ID string, doc map[string]interface{}) error {
+	url := fmt.Sprintf("http://%s:%d/%s/_doc/%s", service.host, service.port, index, ID)
+	bodyRequest, err := json.Marshal(doc)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(bodyRequest))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		return err
+	}
+
+	res, err := service.http.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusCreated {
+		return fmt.Errorf("Failed save document elasticsearch %s", index)
+	}
+	return nil
+}
+
+//Find get list data from elasticsearch
+func (service *ElasticSearchService) Find(index string, size, from int, filter map[string]interface{}) ([]models.ElasticSearch, error) {
+	url := fmt.Sprintf("http://%s:%d/%s/_search?size=%d&from=%d", service.host, service.port, index, size, from)
+	bodyRequest, err := json.Marshal(filter)
+	if err != nil {
+		return []models.ElasticSearch{}, err
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer(bodyRequest))
+	req.Header.Set("Content-Type", "application/json")
+	if err != nil {
+		return []models.ElasticSearch{}, err
+	}
+
+	res, err := service.http.Do(req)
+	if err != nil {
+		return []models.ElasticSearch{}, err
+	}
+
+	var result models.ElasticSearchResult
+	err = json.NewDecoder(res.Body).Decode(&result)
+	if err != nil {
+		return []models.ElasticSearch{}, err
+	}
+
+	return result.Result.List, nil
+}
+
+// Clear delete all index
+func (service *ElasticSearchService) Clear() error {
+	url := fmt.Sprintf("http://%s:%d/_all", service.host, service.port)
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = service.http.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- module/v1/service/news.go
+++ module/v1/service/news.go
@@ -1,1 +1,42 @@
 package services
+
+import (
+	"time"
+
+	models "github.com/yaya-ri/API-news/module/v1/model"
+	requests "github.com/yaya-ri/module/v1/object/request"
+	repository "github.com/yaya-ri/module/v1/repository"
+
+	"github.com/jinzhu/gorm"
+)
+
+//NewsService godoc
+type NewsService struct {
+	DB   *gorm.DB
+	News repository.NewsRepositoryInterface
+}
+
+//NewsServiceHandler godoc
+func NewsServiceHandler(db *gorm.DB) NewsServiceInterface {
+	return &NewsService{
+		DB:   db,
+		News: repository.NewsRepositoryHandler(db),
+	}
+}
+
+//Store service for call repository store
+func (service *NewsService) Store(request requests.News) (models.News, error) {
+	model := models.News{
+		Author:  request.Author,
+		Body:    request.Body,
+		Created: time.Now(),
+	}
+
+	return model, service.News.Store(&model)
+}
+
+//Find service for call repository find
+func (service *NewsService) Find(id uint) (models.News, error) {
+	response, err := service.News.Find(id)
+	return response, err
+}
