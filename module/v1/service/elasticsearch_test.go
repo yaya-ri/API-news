125220d7fd390b3c9ff457c52da5dcdb0356c83a
yaya rizqi isnanda <yaya.rizqi.isnanda@gmail.com>
2020-03-09 18:59:17 +0700

    enhance: unit test service elastic search

--- module/v1/service/elasticsearch_test.go
+++ module/v1/service/elasticsearch_test.go
@@ -1,6 +1,10 @@
 package services
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestCheckIndexExist(t *testing.T) {
 	service := ElasticSearchServiceHandler("localhost", 9207)
@@ -15,5 +19,60 @@ func TestCheckIndexExist(t *testing.T) {
 	}
 	if res != expected {
 		t.Error("ElasticSearchService.IndexExists failed to retrieve expected data")
+	}
+}
+
+func TestCreateIndex(t *testing.T) {
+
+	service := ElasticSearchServiceHandler("localhost", 9207)
+	service.Clear()
+
+	err := service.CreateIndex("newss", map[string]interface{}{
+		"mappings": map[string]interface{}{
+			"properties": map[string]interface{}{
+				"id": map[string]interface{}{
+					"type": "integer",
+				},
+				"created": map[string]interface{}{
+					"type":   "date",
+					"format": "yyyy-MM-dd HH:mm:ss",
+				},
+			},
+		},
+	})
+	if err != nil {
+		t.Error("ElasticSearchService.AddIndex failed with error")
+		t.Errorf(err.Error())
+	}
+}
+
+func TestElasticSearchAddIndex(t *testing.T) {
+
+	service := ElasticSearchServiceHandler("localhost", 9207)
+	service.Clear()
+
+	service.CreateIndex("newss", map[string]interface{}{
+		"mappings": map[string]interface{}{
+			"properties": map[string]interface{}{
+				"id": map[string]interface{}{
+					"type": "integer",
+				},
+				"created": map[string]interface{}{
+					"type":   "date",
+					"format": "yyyy-MM-dd HH:mm:ss",
+				},
+			},
+		},
+	})
+
+	payload := map[string]interface{}{
+		"id":      1,
+		"created": time.Now().Format("2006-01-02 15:04:05"),
+	}
+
+	err := service.Store("newss", fmt.Sprintf("%d", 1), payload)
+	if err != nil {
+		t.Error("ElasticSearchService.AddItem failed with error")
+		t.Errorf(err.Error())
 	}
 }
