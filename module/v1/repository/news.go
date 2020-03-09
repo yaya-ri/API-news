8e8ce1b8cfb0e383106a6893c9cdad5a44457c9c
yaya rizqi isnanda <yaya.rizqi.isnanda@gmail.com>
2020-03-09 15:35:46 +0700

    feature: add news repository store and find

--- module/v1/repository/news.go
+++ module/v1/repository/news.go
@@ -1,1 +1,33 @@
 package repositories
+
+import (
+	models "github.com/yaya-ri/API-news/module/v1/model"
+
+	"github.com/jinzhu/gorm"
+)
+
+//NewsRepository godoc
+type NewsRepository struct {
+	DB        *gorm.DB
+	TableName string
+}
+
+//NewsRepositoryHandler godoc
+func NewsRepositoryHandler(db *gorm.DB) NewsRepositoryInterface {
+	return &NewsRepository{
+		DB:        db,
+		TableName: "news",
+	}
+}
+
+//Store save data news into table news
+func (repo *NewsRepository) Store(model *models.News) error {
+	return repo.DB.Save(model).Error
+}
+
+//Find search data from table news
+func (repo *NewsRepository) Find(id uint) (models.News, error) {
+	response := models.News{}
+	query := repo.DB.Where("id = ?", id).Find(&response)
+	return response, query.Error
+}
